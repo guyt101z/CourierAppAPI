b17ade553b790dd715d4552626c578a560cfbefb
torreta <Luiscampostorreta@gmail.com>
2014-10-18 18:38:05 -0430

    Adding changes to the db:schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018053936) do
+ActiveRecord::Schema.define(version: 20141018225303) do
 
   create_table "agencies", force: true do |t|
     t.string   "name"
     t.string   "phone"
     t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "role"
+    t.string   "cedula"
+    t.string   "name"
+    t.string   "lastname"
+    t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
