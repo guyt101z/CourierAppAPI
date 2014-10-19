b70c4405d3a662dbc500305246e83f3ac92c6f2c
Luis Campos <luiscampostorreta@gmail.com>
2014-10-19 19:41:53 +0000

    adding API keys for athentication

---
+++ db/migrate/20141019194116_create_api_keys.rb
@@ -0,0 +1,11 @@
+class CreateApiKeys < ActiveRecord::Migration
+  def change
+    create_table :api_keys do |t|
+      t.string :access_token
+      t.string :role
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
