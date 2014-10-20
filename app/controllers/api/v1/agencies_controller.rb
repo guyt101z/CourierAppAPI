d49fdeef4487601fab2af90deffe01f4ca4618f8
Luis Campos <luiscampostorreta@gmail.com>
2014-10-20 04:31:15 +0000

    adding versioning branch progress

---
+++ app/controllers/api/v1/agencies_controller.rb
@@ -0,0 +1,69 @@
+module API
+  module V1
+    class AgenciesController < ApplicationController
+      # GET /agencies
+      # GET /agencies.json
+      def index
+        @agencies = Agency.all
+
+        # Aqui estoy haciendo que el api responda en mas de 1 formato
+       # respond_to do |format|
+       #    format.json { render :json => @agencies }
+       #    format.xml { render :xml => @agencies }
+       #  end
+        render json: @agencies
+      end
+
+      # GET /agencies/1
+      # GET /agencies/1.json
+      def show
+        @agency = Agency.find(params[:id])
+
+        render json: @agency, status: :ok
+      end
+
+      # POST /agencies
+      # POST /agencies.json
+      def create
+        @agency = Agency.new(agency_params)
+
+        if @agency.save
+          render json: @agency, status: :created, location: @agency
+        else
+          render json: @agency.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /agencies/1
+      # PATCH/PUT /agencies/1.json
+      def update
+        @agency = Agency.find(params[:id])
+
+        if @agency.update(agency_params)
+          #head :no_content
+          render json: @agency, status: :accepted, location: @agency #sera? status accepted? 
+        else
+          render json: @agency.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /agencies/1
+      # DELETE /agencies/1.json
+      def destroy
+        @agency = Agency.find(params[:id])
+        @agency.destroy
+
+        #head :no_content
+        head :accepted # o deberia dejarlo en not_content
+      end
+
+      private
+
+        def agency_params
+          #params.require(:agency).permit(:name, :phone, :address) #hice lo correcto?
+          params.permit(:name, :phone, :address)
+        end
+
+    end
+  end
+end
---
+++ app/controllers/api/v1/rates_controller.rb
@@ -0,0 +1,68 @@
+module API
+  module V1
+    class RatesController < ApplicationController
+      # GET /rates
+      # GET /rates.json
+      def index
+        @rates = Rate.all
+
+       # Aqui estoy haciendo que el api responda en mas de 1 formato
+       respond_to do |format|
+          format.json { render :json => @rates }
+          format.xml { render :xml => @rates }
+        end
+       # render json: @rates
+      end
+
+      # GET /rates/1
+      # GET /rates/1.json
+      def show
+        @rate = Rate.find(params[:id])
+
+        render json: @rate
+      end
+
+      # POST /rates
+      # POST /rates.json
+      def create
+        @rate = Rate.new(rate_params)
+
+        if @rate.save
+          render json: @rate, status: :created, location: @rate
+        else
+          render json: @rate.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /rates/1
+      # PATCH/PUT /rates/1.json
+      def update
+        @rate = Rate.find(params[:id])
+
+        if @rate.update(rate_params)
+          #head :no_content
+          render json: @rate, status: :accepted, location: @rate #sera? status accepted? 
+        else
+          render json: @rate.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /rates/1
+      # DELETE /rates/1.json
+      def destroy
+        @rate = Rate.find(params[:id])
+        @rate.destroy
+
+        #head :no_content
+        head :accepted # o deberia dejarlo en not_content
+      end
+
+      private
+
+        def rate_params
+          #params.require(:rate).permit(:creator_id, :constant, :K_value)
+          params.permit(:creator_id, :constant, :K_value)
+        end
+    end
+  end
+end
