2ed6d1429b6040b0e58c54c3234cba34bc79fbb8
Luis Campos <luiscampostorreta@gmail.com>
2014-10-20 05:23:09 +0000

    added header validation for email and password for token request

--- app/controllers/api_key_controller.rb
+++ app/controllers/api_key_controller.rb
@@ -1,16 +1,35 @@
 class APIKeyController < ApplicationController
   
   def create
-    #verifico que el usuario haciendo la peticion sea valido
-    
-    
-    @api_key = ApiKey.new(key_params)
-       
-    if @api_key.save
-      render json: @api_key, status: :created, location: @api_key
-    else
-      head :unauthorized
+    #verifico que el usuario haciendo la peticion sea valido, sus datos vienen por el header
+    if request.headers["email"] && request.headers["password"] 
+      @user = User.find_by_email(request.headers["email"])
+      if @user && (request.headers["password"] == @user.password ) 
+          #si el usuario resulto ser real
+        @api_key = ApiKey.new(role:@user.role,user_id:@user.id)  
+        if @api_key.save
+          render json: @api_key, status: :created, location: @api_key
+        else
+          head :unauthorized
+        end
+
+      else #si resulta que falla en la contraseña
+        head :unauthorized
+      end    
+    else #por si viene sin los headers email o password
+      head :bad_request
     end
+    
+    
+    #head :unauthorized unless @user
+    
+#     @api_key = ApiKey.new(key_params)
+       
+#     if @api_key.save
+#       render json: @api_key, status: :created, location: @api_key
+#     else
+#       head :unauthorized
+#     end
   end
   
   
