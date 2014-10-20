2577a6d8b5b52b0e014ec8cbbaf623c4d7a243fb
Luis Campos <luiscampostorreta@gmail.com>
2014-10-20 04:48:57 +0000

    adding token request to the API

---
+++ app/controllers/api_key_controller.rb
@@ -0,0 +1,29 @@
+class APIKeyController < ApplicationController
+  
+  def create
+    @api_key = ApiKey.new(key_params)
+
+    if @api_key.save
+      render json: @api_key, status: :created, location: @api_key
+    else
+      head :unauthorized
+    end
+  end
+  
+  
+  def destroy
+    @api_key = ApiKey.find(params[:id])
+    @api_key.destroy
+
+    #head :no_content
+    head :accepted # o deberia dejarlo en not_content
+  end
+  
+  private
+    
+    def key_params      
+      params.permit(:role, :user_id)
+    end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
         resources :users, except: [:new, :edit]
         resources :rates, except: [:new, :edit]
         resources :packages, except: [:new, :edit]
+  
+        resources :api_key, only: [:create,:destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
